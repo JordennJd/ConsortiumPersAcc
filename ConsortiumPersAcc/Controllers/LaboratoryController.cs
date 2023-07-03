b985343b22abf97540428274c1c843e518474476
Евгения Галеева <zenagaleeva1315@gmail.com>
2023-07-03 10:53:45 +0300

    отключила от бд

--- ConsortiumPersAcc/Controllers/LaboratoryController.cs
+++ ConsortiumPersAcc/Controllers/LaboratoryController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http;
 using ConsortiumPersAcc;
 using ConsortiumPersAcc.Models;
 using ConsortiumPersAcc.Services.DataBaseService;
+using Microsoft.EntityFrameworkCore;
+
 namespace ConsortiumPersAcc.Controllers;
 
 [ApiController]
@@ -17,21 +19,43 @@ public class LaboratoryController : ControllerBase
     public IActionResult GetLabs()
     {
 
-        Sponsor spon1 = new Sponsor() { name = "spon1", id = 1 };
-        Partner part1 = new Partner() { name = "part", id = 1 };
-        List<Partner> parts = new List<Partner>() { part1 };
+        Sponsor spon1 = new Sponsor() { name = "yandexlogo" };
+        Sponsor spon2 = new Sponsor() { name = "gazpromlogo" };
+        Sponsor spon3 = new Sponsor() { name = "gazpromlogo" };
 
-        List<Sponsor> spons = new List<Sponsor>() { spon1 };
-        Laboratory lab1 =new Laboratory(){name = "lab1",place = 1,id=1,Sponsors = spons,Partners = parts};
-        using (var context = new Context())
-        {
-            context.Laboratories.Add(lab1);
-            context.SaveChanges();
-            Console.WriteLine(context.Laboratories.Count());
-        }
+        Sponsor spon4 = new Sponsor() { name = "gazpromlogo" };
+
+        Sponsor spon5 = new Sponsor() { name = "gazpromlogo" };
+
+
+
+        List<Sponsor> spons1 = new List<Sponsor>() { spon1, new Sponsor() { name = "sponsor" } };
+        List<Sponsor> spons2 = new List<Sponsor>() { spon2, new Sponsor() { name = "gazpromlogo" }, new Sponsor() { name = "gazpromlogo" }, new Sponsor() { name = "gazpromlogo" }, new Sponsor() { name = "gazpromlogo" } };
+        List<Sponsor> spons3 = new List<Sponsor>() { spon3 };
+        List<Sponsor> spons4 = new List<Sponsor>() { spon4 };
+        Laboratory lab1 = new Laboratory() { name = "lab1", id = 1, place = 1, Sponsors = spons1 };
+        Laboratory lab2 = new Laboratory() { name = "lab2", id = 2, place = 2, Sponsors = spons2 };
+        Laboratory lab3 = new Laboratory() { name = "lab3", id = 3, place = 3, Sponsors = spons3 };
+        Laboratory lab4 = new Laboratory() { name = "lab4", id = 4, place = 4, Sponsors = spons4 };
 
         Laboratories.Add(lab1);
+        Laboratories.Add(lab2);
+        Laboratories.Add(lab3);
+        Laboratories.Add(lab4);
 
+
+        // using (var context = new Context())
+        // {
+        //     int place = 1;
+
+        //     foreach (var lab in context.Laboratories.Include("Sponsors"))
+        //     {
+        //         lab.place = place; 
+        //         Laboratories.Add(lab);
+        //         place++;
+
+        //     }
+        // }
         return StatusCode(StatusCodes.Status200OK, Laboratories);
     }
 
