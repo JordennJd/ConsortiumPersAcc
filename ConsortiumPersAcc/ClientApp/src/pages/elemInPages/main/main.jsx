d2f8f63f5a6ddddeb5228e3cd2ffc30a1873976a
Евгения Галеева <zenagaleeva1315@gmail.com>
2023-07-03 10:59:38 +0300

    исправления

--- ConsortiumPersAcc/ClientApp/src/pages/elemInPages/main/main.jsx
+++ ConsortiumPersAcc/ClientApp/src/pages/elemInPages/main/main.jsx
@@ -1,164 +1,81 @@
 import Laboratory from './laboratory/laboratory';
 import style from './main.module.css';
 import LeaderLine from "react-leader-line";
-import { useRef, useEffect, createRef,useState } from 'react';
+import React, { Component } from 'react';
 
-import Laboratory_copy from './laboratory/laboratory_copy';
 
-const laboratoryList = [
-    {
-        id: '1',
-        name: 'Лаборатория технологического предпринимательства',
-        sponsors: [
-            { id: 1, name: 'MegafonLogo' },
-            { id: 2, name: 'YandexLogo' },
-            { id: 3, name: 'GazPromLogo' },
-            { id: 4, name: 'UniversityNTILogo' },
-            { id: 5, name: 'KirovskiiZavodLogo' }
-        ],
-        partners: [
-            {id:1, name:'GazProm'},
-            {id:2, name:'Megafon'},
-            {id:3, name:'SilovMash'},
-            {id:4, name:'Rosseti'}
-        ]
-    },
-    {
-        id: '2', name: 'Лаборатория интернета вещей', sponsors: [
-            { id: 1, name: 'MegafonLogo' },
-            { id: 2, name: 'YandexLogo' },
-            { id: 3, name: 'GazPromLogo' },
-            { id: 4, name: 'UniversityNTILogo' },
-            { id: 5, name: 'KirovskiiZavodLogo' }
-        ],
-        partners: [
-            {id:1, name:"nationalInstruments"},
-            {id:2, name:"kuka"}
-        ]
-    },
-    {
-        id: '3', name: 'Лаборатория робототехники', sponsors: [
-            { id: 1, name: 'MegafonLogo' },
-            { id: 2, name: 'YandexLogo' },
-            { id: 3, name: 'GazPromLogo' },
-            { id: 4, name: 'UniversityNTILogo' },
-            { id: 5, name: 'KirovskiiZavodLogo' }
-        ],
-        partners: [
-            {id:1, name:"nationalInstruments"},
-            {id:2, name:"kuka"}
-        ]
-        
-    },
-    {
-        id: '4', name: 'Лаборатория беспилотных авиационных систем', sponsors: [
-            { id: 1, name: 'MegafonLogo' },
-            { id: 2, name: 'YandexLogo' },
-            { id: 3, name: 'GazPromLogo' },
-            { id: 4, name: 'UniversityNTILogo' },
-            { id: 5, name: 'KirovskiiZavodLogo' }
-        ],
-        partners: [
-            {id:1, name:'GazProm'},
-            {id:2, name:'Megafon'},
-            {id:3, name:'SilovMash'},
-            {id:4, name:'Rosseti'}
-        ]
-    },
-    {
-        id: '5', name: 'Лаборатория искусственного интеллекта', sponsors: [
-            { id: 1, name: 'MegafonLogo' },
-            { id: 2, name: 'YandexLogo' },
-            { id: 3, name: 'GazPromLogo' },
-            { id: 4, name: 'UniversityNTILogo' },
-            { id: 5, name: 'KirovskiiZavodLogo' }
-        ],
-        partners: [
-            {id:1, name:'GazProm'},
-            {id:2, name:'Megafon'},
-            {id:3, name:'SilovMash'},
-            {id:4, name:'Rosseti'}
-        ]
-    },
-    {
-        id: '6', name: 'Лаборатория кибербезопасности ГУАП-Infowatch', sponsors: [
-            { id: 1, name: 'MegafonLogo' },
-            { id: 2, name: 'YandexLogo' },
-            { id: 3, name: 'GazPromLogo' },
-            { id: 4, name: 'UniversityNTILogo' },
-            { id: 5, name: 'KirovskiiZavodLogo' }
-        ],
-        partners: [
-            {id:1, name:'GazProm'},
-            {id:2, name:'Megafon'},
-            {id:3, name:'SilovMash'},
-            {id:4, name:'Rosseti'}
-        ]
+export class Main extends Component {
+    static displayName = Main.name;
+    constructor(props) {
+        super(props);
+        this.state = { LaboratoryList: [], loading: true };
     }
-];
+    
+    componentDidMount() {
+        this.populateWeatherData();
 
-const mainStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(6, 1fr)',
-    gridTemplateRows: 'repeat(' + laboratoryList.at(-1).id + ', 100px 1fr) 100px',
-    alignItems: 'stretch',
-    justifyItems: 'center',
-};
+    }
 
-
-
-const Main = () => {
-    const [LaboratoryList, setLaboratory] = useState([])
-    window.addEventListener('load', () => {
-        fetch("api/Laboratory/GetLabs")
-            .then(response => {
-                return response.json()
-            })
-            .then(responseJson => {
-                setLaboratory(responseJson)
-            })
-    })
-    console.log(LaboratoryList)
-    setTimeout(() => {
+    static renderForecastsTable(LaboratoryList) {
+        
+        const mainStyle = {
+            display: 'grid',
+            gridTemplateColumns: 'repeat(6, 1fr)',
+            gridTemplateRows: 'repeat(' + LaboratoryList.length + ', 100px 1fr) 100px',
+            alignItems: 'stretch',
+            justifyItems: 'center',
+        };
+        console.log(LaboratoryList)
         const lineOptions = {
             endPlug: "behind",
             path: "straight",
             color: "rgba(255, 255, 255, 0.5)"
         };
-        for (let i = 1; i < LaboratoryList.length; i++) {
-            new LeaderLine(document.getElementsByClassName("ref" + i)[0], document.getElementsByClassName("ref" + (i+1))[0], lineOptions);
-        }
-    }, 50);
+        setTimeout(()=>{for (let i = 1; i < LaboratoryList.length; i++) {
+            new LeaderLine(document.getElementsByClassName("ref" + i)[0],
+                document.getElementsByClassName("ref" + (i + 1))[0], lineOptions)
+        }},100)
 
-    let i = 1
-    return (
-        <main className={style.mainContent} style={mainStyle}>
-            {LaboratoryList.map(laboratory =>
-                <Laboratory
-                    id={laboratory.id}
-                    ref={laboratoryList.ref}
-                    nameLaboratory={laboratory.name}
-                    sponsors={laboratory.sponsors}
-                    partners={laboratoryList.partners}
-                />
-            )}
-            {/*laboratoryList.map(laboratory =>
-                <Laboratory_copy
-                    id={laboratory.id}
-                    ref={laboratoryList.ref}
-                    nameLaboratory={laboratory.name}
-                    sponsors={laboratory.sponsors}
-                    partners={laboratoryList.partners}
-                />
-            )*/}
-            {/*
-        элементы - лаборатории с props
-            будут передаваться координаты центра или типо того
-        элемент
-            линия между элементами-лаборатории
-        */}
-        </main >
-    );
+        return (
+            
+            <main className={style.mainContent} style={mainStyle}>
+
+            
+                {LaboratoryList.map(laboratory =>
+                    <Laboratory
+                        id={laboratory.place}
+                        ref={LaboratoryList.ref}
+                        nameLaboratory={laboratory.name}
+                        sponsors={laboratory?.sponsors}
+                        // partners={LaboratoryList.partners}
+                    />
+                )}
+            </main>
+        )
+    }
+
+    render() {
+        
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Main.renderForecastsTable(this.state.LaboratoryList);
+        return (
+            
+            <div>
+                
+                <h1 id="tableLabel"></h1>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateWeatherData() {
+        
+        const response = await fetch('api/Laboratory/GetLabs');
+        const data = await response.json();
+        this.setState({ LaboratoryList: data, loading: false });
+    }
 }
 
-export default Main;
+
+
--- ConsortiumPersAcc/ClientApp/src/pages/laboratorySelection/laboratorySelection.jsx
+++ ConsortiumPersAcc/ClientApp/src/pages/laboratorySelection/laboratorySelection.jsx
@@ -1,6 +1,6 @@
 import Footer from '../elemInPages/footer/footer';
 import Header from '../elemInPages/header/header';
-import Main from '../elemInPages/main/main';
+import { Main } from '../elemInPages/main/main';
 import MainTitle from '../elemInPages/mainTitle/mainTitle';
 
 import style from './laboratorySelection.module.css';
@@ -8,7 +8,7 @@ import style from './laboratorySelection.module.css';
 const LaboratorySelection = () => {
     return (
         <div className={style.laboratorySelection}>
-            <Header style={"header_normal"}/>
+            <Header style={"header_normal"} />
             <MainTitle />
             <Main />
             <Footer />
